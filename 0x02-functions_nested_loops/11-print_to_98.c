fca3f28d68b548a18531e112f8ffc334eaf3a2bf
Elpastore <abdoulayesadio1997@gmail.com>
2023-02-22 23:50:33 +0000

    11. 98 Battery Street, the OG3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * print_to_98 -prints all natural numbers from n to 98
  * @n: started number
@@ -18,7 +18,7 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d, " ,n);
+			printf("%d, ", n);
 			n--;
 		}
 	}
