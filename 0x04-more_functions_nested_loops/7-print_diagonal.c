ff9219faa3d27d61779d6bbf3b72c5d141880623
Elpastore <abdoulayesadio1997@gmail.com>
2023-02-23 17:00:51 +0000

    task 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line -function that draws a straight line in the terminal.
+ * print_diagonal -function that draws a straight line in the terminal.
  * @n: number of times
  */
 void print_diagonal(int n)
