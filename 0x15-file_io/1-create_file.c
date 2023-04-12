2d57686f8e78cf8015ceb317a95a36dda4b476f2
Elpastore <abdoulayesadio1997@gmail.com>
2023-04-12 16:51:11 +0000

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * create_file - function that creates a file.
  * @filename :  file to be created
@@ -8,23 +7,33 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp, w, len;
+	int fp, w, len = 0;
 
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+
 	if (fp == -1)
 	{
 		return (-1);
 	}
-	len = strlen(text_content);
-	w = write(fp, text_content, len);
-	if (w == -1)
+
+	if (text_content != NULL)
 	{
-		return (-1);
+
+		while (text_content[len])
+		{
+			len++;
+		}
+		w = write(fp, text_content, len);
+		if (w == -1)
+		{
+			return (-1);
+		}
 	}
+
 	close(fp);
 	return (1);
 }
