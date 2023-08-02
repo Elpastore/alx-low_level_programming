591c157ebccad76ad4bfa5acfe9656f36ae080d9
elpastore <abdoulayesadio1997@gmail.com>
2023-08-02 13:25:57 +0000

    task1.1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,7 +10,9 @@ void _print_rev_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
