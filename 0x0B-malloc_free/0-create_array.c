4c06b433e62e73e39a768834aa66bccc532e1daa
elpastore <abdoulayesadio1997@gmail.com>
2023-08-07 12:35:08 +0000

    task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars
+ * @c: a char
+ * @size: size of array
+ * Return: a pointer of the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(size);
+
+	if ( p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This project is about  dynamic allocation using malloc and free
