b2e1f79e16587f323835569b292cadab3e0592f1
elpastore <abdoulayesadio1997@gmail.com>
2023-08-14 21:18:57 +0000

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a structure that store info about dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
