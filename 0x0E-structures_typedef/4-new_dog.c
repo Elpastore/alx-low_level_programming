74bc691382a7fd4082a83ed8b6311b7cdde61ecd
elpastore <abdoulayesadio1997@gmail.com>
2023-08-15 00:11:36 +0000

    taks4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,6 +46,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cpy_name, *cpy_owner;
 	int len_name, len_owner;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 	len_name = _strlen(name);
 	len_owner = _strlen(owner);
 
