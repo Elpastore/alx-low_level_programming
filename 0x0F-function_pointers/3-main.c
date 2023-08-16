96417e5e2b779291d4fb98425d9cfed60f2c5596
elpastore <abdoulayesadio1997@gmail.com>
2023-08-16 22:23:19 +0000

    task4

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+/**
+ * main -the entry point of the program
+ * @argc: number of argument passed
+ * @argv: array of char that store argument
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2]; /*this is the specific operator*/
+
+	if (get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((num2 == 0 && *op == '*') || (num2 == 0 && *op == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = get_op_func(op)(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add  - function that perform addition operation
+ * @a: integer number
+ * @b: interger number
+ * Return: the result of operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub  - function that perform sustraction operation
+ * @a: integer number
+ * @b: interger number
+ * Return: the result of operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul  - function that perform multiplication operation
+ * @a: integer number
+ * @b: interger number
+ * Return: the result of operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div  - function that perform division operation
+ * @a: integer number
+ * @b: interger number
+ * Return: the result of operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod  - function that perform modulo operation
+ * @a: integer number
+ * @b: interger number
+ * Return: the remainde of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
