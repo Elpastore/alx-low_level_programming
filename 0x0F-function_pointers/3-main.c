be82111b1f7511f23f1bcaa860da8b2072307f94
elpastore <abdoulayesadio1997@gmail.com>
2023-08-16 22:54:16 +0000

    task4.5

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	op = argv[2]; /*this is the specific operator*/
 
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
