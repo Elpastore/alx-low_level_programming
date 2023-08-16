e4aa900ea44d8e39a7f9bdb0afb97110aa6fafbe
elpastore <abdoulayesadio1997@gmail.com>
2023-08-16 23:00:50 +0000

    see

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,10 +16,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && (*(ops[i].op) != *s)
+)
 	{
-		if (*(ops[i].op) != *s)
-			return (NULL);
 		i++;
 	}
 	return (ops[i].f);
