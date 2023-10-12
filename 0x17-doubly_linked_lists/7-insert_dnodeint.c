4ec6fa109d66300bc26b2c5409bb2e1a08637354
elpastore <abdoulayesadio1997@gmail.com>
2023-10-12 21:09:57 +0000

    task7_v3

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = *h;
 		if (*h != NULL)
 			(*h)->prev = new_node;
-		new_node->prev = NULL;
 		*h = new_node;
 		return (new_node);
 	}
