7c9ed3b0308282df59f430133eb90f8fdc4cfe9c
elpastore <abdoulayesadio1997@gmail.com>
2023-10-16 22:11:22 +0000

    last_task_v2

--- 0x18-dynamic_libraries/always_win.c
+++ 0x18-dynamic_libraries/always_win.c
@@ -5,7 +5,7 @@
 
 int printf(const char *format, ...)
 {
-	char *message = "Congratulations, you win the Jackpot!";
+	char *message = "Congratulations, you win the Jackpot!\n";
 	char *numbers = "9 8 10 24 75 - 9\n";
 
 	write(1, numbers, strlen(numbers));
