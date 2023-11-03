807c28158f2327710cfe1fb4dbc667c14f3de59f
elpastore <abdoulayesadio1997@gmail.com>
2023-11-03 21:36:44 +0000

    task3 version4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -67,12 +67,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
