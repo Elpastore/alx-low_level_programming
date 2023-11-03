9c635e1583c9fb4e94a9591a08ce32296ef1b67f
elpastore <abdoulayesadio1997@gmail.com>
2023-11-03 21:35:39 +0000

    task3 version3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,36 @@
 #include "hash_tables.h"
 
 /**
+ * insert_node - function that insert a node
+ * @key: the key
+ * @value: the value associed with the key
+ * Return: the new_node or NULL if failed
+ */
+hash_node_t *insert_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
+}
+
+/**
  * hash_table_set - function that add an element
  * @ht: the hash_table
  * @key: the key
@@ -10,29 +40,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node, *current;
+	char *dup_value;
 
 	if (ht == NULL || ht->size == 0 || key == NULL || value == NULL)
 		return (0);
 	if (strcmp(key, "") == 0)
 		return (0);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(key, current->key) == 0)
+		{
+			dup_value = strdup(value);
+			if (dup_value == NULL)
+				return (0);
+			free(current->value);
+			current->value = dup_value;
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = insert_node(key, value);
 	if (new_node == NULL)
-	{
 		return (0);
-	}
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
 	else
